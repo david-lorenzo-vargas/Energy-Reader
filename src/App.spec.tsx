23148af03beab99d7543da55d9ae5e9abfbe8532
david-lorenzo-vargas <dlorenzo.05@gmail.com>
2024-03-07 15:14:28 +0000

    app component and testing

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,9 +1,23 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
+const renderApp = () => render(<App />);
+
 describe("App", () => {
-  test("includes the header", () => {
-    const app = render(<App />);
-    app.getByText("Meter Readings");
+  it('should open SeeAllReadingsModal modal', () => {
+    renderApp();
+
+    fireEvent.click(screen.getByRole('button', { name: 'See all' }));
+
+    expect(screen.getByTestId('modal-Previous meter readings')).toBeInTheDocument();
+  });
+  it('should open WarningModal modal', () => {
+    renderApp();
+
+    fireEvent.click(screen.getByRole('button', { name: 'Add predicted reading' }));
+
+    expect(screen.getByText('Are you sure you want to add')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Cancel' })).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Accept' })).toBeInTheDocument();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,111 @@
-import { useState } from "react";
-import { MeterReading } from "./types";
-import "./styles.css";
+import { ReactElement, useCallback, useEffect, useState } from "react";
 
-export default function App() {
-  const [readings, setReadings] = useState<MeterReading[]>([]);
+import Meter from "./organisms/Meter";
+import Prediction from "./molecules/Prediction";
+import Readings from "./organisms/Readings";
+import SeeAllReadingsModal from "./molecules/Modals/SeeAllReadingsModal";
+import WarningModal from "./molecules/Modals/WarningModal";
 
-  const readingListItems = readings.map((reading) => (
-    <li key={reading.value}>
-      {reading.value} - {reading.source}
-    </li>
-  ));
+import { MeterReading, MeterReadingSourceEnum } from "./types";
+
+import { fetchData } from "./Mock/mockReadings";
+
+import { sortReadingsByDate } from "./util/sortReadingsByDate";
+
+const App = (): ReactElement => {
+  const [previousReadings, setPreviousReadings] = useState<MeterReading[]>([]);
+  const [readingsForPrediction, setReadingsForPrediction] = useState<MeterReading[]>([]);
+
+  const [newReading, setNewReading] = useState<MeterReading | undefined>(undefined);
+
+  const [seeAll, setSeeAll] = useState<boolean>(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res: MeterReading[] = await fetchData();
+
+        const sortedResponse = sortReadingsByDate(res);
+        const arrCopy = [...sortedResponse];
+
+        setPreviousReadings(() => sortedResponse);
+        setReadingsForPrediction(() => arrCopy.slice(0, 4));
+      } catch (error) {
+        throw new Error('Oops! there was a problem');
+      }
+    })();
+  }, []);
+
+  const onAddNewReading = useCallback((r: number[]) => {
+    const newReading = {
+      value: r.join(''),
+      date: new Date(),
+      source: MeterReadingSourceEnum.customer,
+      uuid: '',
+    };
+
+    setNewReading(() => newReading);
+  }, []);
+
+  const onAddPrediction = useCallback((p: MeterReading) => {
+    setNewReading(() => p);
+  }, []);
+
+  const onToggleSeeAll = useCallback(() => {
+    setSeeAll((a: boolean) => !a);
+  }, []);
+
+  const onAcceptNewReading = useCallback(() => {
+    if (newReading) {
+      setPreviousReadings((p: MeterReading[]) => [newReading, ...p]);
+      setReadingsForPrediction(() => {
+        const readings = [...previousReadings].slice(0, 3);
+
+        return [newReading, ...readings];
+      });
+      setNewReading(() => undefined);
+    }
+  }, [newReading, previousReadings]);
 
   return (
-    <div className="App">
-      <h1>Meter Readings</h1>
-      <p>Enter a new meter reading:</p>
-      <input className="input"></input>
-      <button>Submit</button>
-      <p className="error" style={{ display: "none" }}>
-        This is an invalid meter reading.
-      </p>
-      <h2>Predicted usage next month</h2>
-      <p>Coming soon</p>
-      <h2>Previous meter readings</h2>
-      <ul>{readingListItems}</ul>
+    <div className="h-screen w-screen md:bg-darkBlueGray md:bg-opacity-40">
+      <div className="flex flex-col h-scren md:flex-row w-full justify-center">
+        <div className="w-full bg-orange md:bg-transparent flex flex-col items-center py-5">
+          <div className="md:w-1/2 w-60P">
+            <Meter
+              onAddNewReading={onAddNewReading}
+              lastReading={Number(previousReadings[0]?.value)}
+            />
+          </div>
+        </div>
+        <div className="w-full">
+          <div className="w-full md:w-1/2">
+            <Readings
+              readings={previousReadings}
+              onSeeAllReadings={onToggleSeeAll}
+            />
+          </div>
+          <div className="w-full">
+            <Prediction
+              readings={readingsForPrediction}
+              onAddPrediction={onAddPrediction}
+            />
+          </div>
+        </div>
+      </div>
+      <SeeAllReadingsModal
+        onToggleSeeAll={onToggleSeeAll}
+        mounted={seeAll}
+        readings={previousReadings}
+      />
+      <WarningModal
+        mounted={!!newReading?.hasOwnProperty('value')}
+        reading={newReading?.value ?? ''}
+        onCloseWarningModal={() => setNewReading(undefined)}
+        onAcceptNewReading={onAcceptNewReading}
+      />
     </div>
   );
-}
+};
+
+export default App;

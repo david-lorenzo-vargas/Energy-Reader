9695925a47eda8c8b130c30e06f090cfed35a99d
david-lorenzo-vargas <dlorenzo.05@gmail.com>
2024-03-19 11:07:13 +0000

    header

--- src/organisms/Meter/Header/index.tsx
+++ src/organisms/Meter/Header/index.tsx
@@ -4,11 +4,8 @@ import Logo from "../../../icons/Logo";
 
 const Header = (): ReactElement => {
   return (
-    <div data-testid="logo" className="relative h-20">
-      <div className="absolute z-10 text-lightGray">
-        <Logo size="100"/>
-      </div>
-      <div className="bg-orange w-100 h-10 absolute bottom-2"/>
+    <div data-testid="logo">
+      <Logo size="100"/>
     </div>
   );
 };
